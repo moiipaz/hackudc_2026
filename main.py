5baade58d6e91ffe5a892385a6d1c866dbfdebc1
moiipaz <moira.paz.fernandez@gmail.com>
2026-02-28 04:43:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,22 @@ from datetime import datetime
 import json
 import os
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://127.0.0.1:8000",
+        "http://localhost:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 DB_FILE = "notas.json"
 
